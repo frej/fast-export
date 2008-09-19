02bc08886f3e2d07ae18e5bceda57f8b1a6694e8
Rocco Rutte <pdmef@gmx.net>
2008-09-19 08:01:53 +0200

    hg-fast-export: Catch up with mercurial crew API changes
    Signed-off-by: Rocco Rutte <pdmef@gmx.net>

--- hg-fast-export.py
+++ hg-fast-export.py
@@ -330,7 +330,10 @@ def hg2git(repourl,m,marksfile,headsfile,tipfile,authors={},sob=False,force=Fals
   if not verify_heads(ui,repo,heads_cache,force):
     return 1
 
-  tip=repo.changelog.count()
+  try:
+    tip=repo.changelog.count()
+  except AttributeError:
+    tip=len(repo)
 
   min=int(state_cache.get('tip',0))
   max=_max
