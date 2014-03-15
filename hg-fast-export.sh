cb55f7443778bbabb81029f7671a6a4b152f0a56
Kyle J. McKay <mackyle@gmail.com>
2014-03-14 21:01:35 -0700

    Revert "Make bashism conditional on the shell being bash"
    This reverts commit aaccfba9a0cfcfdd4db1e629b50d65b1c479b64f.

--- hg-fast-export.sh
+++ hg-fast-export.sh
@@ -3,9 +3,7 @@
 # Copyright (c) 2007, 2008 Rocco Rutte <pdmef@gmx.net> and others.
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
-if [ -n "$BASH" ]; then
-    set -o pipefail
-fi
+set -o pipefail
 
 ROOT="`dirname $0`"
 REPO=""
