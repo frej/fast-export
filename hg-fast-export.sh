f64c10ba1406c484704e75368ee1e781f13f4de0
Frej Drejhammar <frej.drejhammar@gmail.com>
2014-09-23 15:01:02 +0200

    Do not rely on "type -p"
    Apparently dash (used as bin/sh on Ubuntu) does not support type -p.
    
    Thanks to Esben Madsen for noticing and suggesting a fix.

--- hg-fast-export.sh
+++ hg-fast-export.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2007, 2008 Rocco Rutte <pdmef@gmx.net> and others.
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
-ROOT="`dirname "$(type -p $0)"`"
+ROOT="$(dirname "$(which "$0")")"
 REPO=""
 PFX="hg2git"
 SFX_MAPPING="mapping"
