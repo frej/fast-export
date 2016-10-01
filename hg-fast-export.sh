frej/fast-export

commit 6cf9397bd6aadf659efb5104af4d3f37b0753d73

Do not rely on git internals, support Git >= 2.10
Fast-export has traditionally sourced the internal git-sh-setup from
Git, following the release of Git 2.10 this no longer works. Fast-export
only uses the functionality of git-sh-setup for two things: cd:ing to
the git repo dir and setting up the GIT_REPO environment variable. To
future-proof fast-export start doing what we need by hand in
fast-export.

Acknowledgments to Louis Sautier who reported the problem and tested the
fix.

hg-fast-export.sh
@@ -48,8 +48,10 @@ case "$1" in
       echo "$LONG_USAGE"
       exit 0
 esac
-. "$(git --exec-path)/git-sh-setup"
-cd_to_toplevel
+
+cd $(git rev-parse --show-toplevel) \
+    || (echo "Could not find git repo" ; exit 1)
+GIT_DIR=$(git rev-parse --git-dir) || exit 1
 
 while case "$#" in 0) break ;; esac
 do

hg-reset.sh
@@ -24,8 +24,9 @@ Options:
 	-r	Mercurial repository to use
 "
 
-. "$(git --exec-path)/git-sh-setup"
-cd_to_toplevel
+cd $(git rev-parse --show-toplevel) \
+    || (echo "Could not find git repo" ; exit 1)
+GIT_DIR=$(git rev-parse --git-dir) || exit 1
 
 while case "$#" in 0) break ;; esac
 do

commit 1d0f6cb7caa74247ab9d520f3c021b7d6ea88bf2

Fix broken support for bare repositories
The change in 6cf9397bd6aadf659efb5104af4d3f37b0753d73 broke support for
bare repositories. In a bare repo git rev-parse --show-toplevel would
return an empty string and cwd would then be changed to the user's home
directory. In the home directory git rev-parse --git-dir would either
fail or return an unrelated repo.

Problem reported by Ralf Rösch.

hg-fast-export.sh
@@ -49,9 +49,15 @@ case "$1" in
       exit 0
 esac
 
-cd $(git rev-parse --show-toplevel) \
+IS_BARE=$(git rev-parse --is-bare-repository) \
     || (echo "Could not find git repo" ; exit 1)
-GIT_DIR=$(git rev-parse --git-dir) || exit 1
+if test "z$IS_BARE" != ztrue; then
+   # This is not a bare repo, cd to the toplevel
+   TOPLEVEL=$(git rev-parse --show-toplevel) \
+       || (echo "Could not find git repo toplevel" ; exit 1)
+   cd $TOPLEVEL || exit 1
+fi
+GIT_DIR=$(git rev-parse --git-dir) || (echo "Could not find git repo" ; exit 1)
 
 while case "$#" in 0) break ;; esac
 do

hg-reset.sh
@@ -24,9 +24,15 @@ Options:
 	-r	Mercurial repository to use
 "
 
-cd $(git rev-parse --show-toplevel) \
+IS_BARE=$(git rev-parse --is-bare-repository) \
     || (echo "Could not find git repo" ; exit 1)
-GIT_DIR=$(git rev-parse --git-dir) || exit 1
+if test "z$IS_BARE" != ztrue; then
+   # This is not a bare repo, cd to the toplevel
+   TOPLEVEL=$(git rev-parse --show-toplevel) \
+       || (echo "Could not find git repo toplevel" ; exit 1)
+   cd $TOPLEVEL || exit 1
+fi
+GIT_DIR=$(git rev-parse --git-dir) || (echo "Could not find git repo" ; exit 1)
 
 while case "$#" in 0) break ;; esac
 do