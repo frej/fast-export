frej/fast-export

commit e200cec39fac8a8ebb1c7d7690c00d5cb15f0c30

Adapt to changes in Mercurial 4.6
Starting with Mercurial 4.6 repo.lookup() no longer accepts raw hashes
for lookups.

hg-fast-export.py
@@ -4,6 +4,7 @@
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
 from mercurial import node
+from mercurial.scmutil import revsymbol
 from hg2git import setup_repo,fixup_user,get_branch,get_changeset
 from hg2git import load_cache,save_cache,get_git_sha1,set_default_branch,set_origin_name
 from optparse import OptionParser
@@ -78,7 +79,7 @@ def get_filechanges(repo,revision,parents,mleft):
   l,c,r=[],[],[]
   for p in parents:
     if p<0: continue
-    mright=repo.changectx(p).manifest()
+    mright=revsymbol(repo,str(p)).manifest()
     l,c,r=split_dict(mleft,mright,l,c,r)
   l.sort()
   c.sort()
@@ -210,7 +211,7 @@ def export_commit(ui,repo,revision,old_marks,max,count,authors,
   wr(desc)
   wr()
 
-  ctx=repo.changectx(str(revision))
+  ctx=revsymbol(repo,str(revision))
   man=ctx.manifest()
   added,changed,removed,type=[],[],[],''
 
@@ -225,7 +226,7 @@ def export_commit(ui,repo,revision,old_marks,max,count,authors,
       # later non-merge revision: feed in changed manifest
       # if we have exactly one parent, just take the changes from the
       # manifest without expensively comparing checksums
-      f=repo.status(repo.lookup(parents[0]),revnode)[:3]
+      f=repo.status(parents[0],revnode)[:3]
       added,changed,removed=f[1],f[0],f[2]
       type='simple delta'
     else: # a merge with two parents
@@ -262,7 +263,7 @@ def export_note(ui,repo,revision,count,authors,encoding,is_first):
   if is_first:
     wr('from refs/notes/hg^0')
   wr('N inline :%d' % (revision+1))
-  hg_hash=repo.changectx(str(revision)).hex()
+  hg_hash=revsymbol(repo,str(revision)).hex()
   wr('data %d' % (len(hg_hash)))
   wr_no_nl(hg_hash)
   wr()

hg2git.py
@@ -4,6 +4,9 @@
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
 from mercurial import hg,util,ui,templatefilters
+from mercurial import error as hgerror
+from mercurial.scmutil import revsymbol,binnode
+
 import re
 import os
 import sys
@@ -69,7 +72,15 @@ def get_branch(name):
   return name
 
 def get_changeset(ui,repo,revision,authors={},encoding=''):
-  node=repo.lookup(revision)
+  # Starting with Mercurial 4.6 lookup no longer accepts raw hashes
+  # for lookups. Work around it by changing our behaviour depending on
+  # how it fails
+  try:
+    node=repo.lookup(revision)
+  except hgerror.ProgrammingError:
+    node=binnode(revsymbol(repo,str(revision))) # We were given a numeric rev
+  except hgerror.RepoLookupError:
+    node=revision # We got a raw hash
   (manifest,user,(time,timezone),files,desc,extra)=repo.changelog.read(node)
   if encoding:
     user=user.decode(encoding).encode('utf8')

commit ba601bbf19c7188b6e2a322b63ba66b3dd7fa67d

Restore support for mercurial<4.6
e200cec39fac8a8ebb1c7d7690c00d5cb15f0c30 broke mercurial<4.6 support,
which was a Bad Thing. Linuxy types particularly are used to being able
to use the mercurial package from their OS package manager, rather than
needing to mess around with installing extra packages from pip (which is
normally a disaster if you’re not a Python programmer and used to
messing around with virtualenvs or similar), and almost none of those
have Mercurial 4.6 yet. (Ubuntu, for example, is on 4.5.3 in 18.04 LTS,
and 3.7.3 in 16.04 LTS. So probably having 4.6 or later is about two and
a half to four years off!)

The differences in Mercurial 4.6 are small enough that restoring support
is not difficult, and it makes it work for everyone, so it’s worth
doing.

Fixes #132.

hg-fast-export.py
@@ -4,7 +4,13 @@
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
 from mercurial import node
-from mercurial.scmutil import revsymbol
+try:
+  from mercurial.scmutil import revsymbol
+  HG46 = True
+except ImportError:
+  def revsymbol(repo,revision):
+    return repo.changectx(revision)
+  HG46 = False
 from hg2git import setup_repo,fixup_user,get_branch,get_changeset
 from hg2git import load_cache,save_cache,get_git_sha1,set_default_branch,set_origin_name
 from optparse import OptionParser
@@ -239,7 +245,7 @@ def export_commit(ui,repo,revision,old_marks,max,count,authors,
       # later non-merge revision: feed in changed manifest
       # if we have exactly one parent, just take the changes from the
       # manifest without expensively comparing checksums
-      f=repo.status(parents[0],revnode)[:3]
+      f=repo.status(parents[0] if HG46 else repo.lookup(parents[0]),revnode)[:3]
       added,changed,removed=f[1],f[0],f[2]
       type='simple delta'
     else: # a merge with two parents

hg2git.py
@@ -4,8 +4,12 @@
 # License: MIT <http://www.opensource.org/licenses/mit-license.php>
 
 from mercurial import hg,util,ui,templatefilters
-from mercurial import error as hgerror
-from mercurial.scmutil import revsymbol,binnode
+try:
+  from mercurial import error as hgerror
+  from mercurial.scmutil import revsymbol,binnode
+  HG46 = True
+except ImportError:
+  HG46 = False
 
 import re
 import os
@@ -72,15 +76,18 @@ def get_branch(name):
   return name
 
 def get_changeset(ui,repo,revision,authors={},encoding=''):
-  # Starting with Mercurial 4.6 lookup no longer accepts raw hashes
-  # for lookups. Work around it by changing our behaviour depending on
-  # how it fails
-  try:
+  if HG46:
+    # Starting with Mercurial 4.6 lookup no longer accepts raw hashes
+    # for lookups. Work around it by changing our behaviour depending on
+    # how it fails
+    try:
+      node=repo.lookup(revision)
+    except hgerror.ProgrammingError:
+      node=binnode(revsymbol(repo,str(revision))) # We were given a numeric rev
+    except hgerror.RepoLookupError:
+      node=revision # We got a raw hash
+  else:
     node=repo.lookup(revision)
-  except hgerror.ProgrammingError:
-    node=binnode(revsymbol(repo,str(revision))) # We were given a numeric rev
-  except hgerror.RepoLookupError:
-    node=revision # We got a raw hash
   (manifest,user,(time,timezone),files,desc,extra)=repo.changelog.read(node)
   if encoding:
     user=user.decode(encoding).encode('utf8')